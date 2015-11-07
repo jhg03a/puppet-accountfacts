8a2859ce7e0f549fa47cb0befdb2aa5dbedebebc
Jonathan Gray <jhg03a@acu.edu>
2015-11-06 18:26:32 -0600

    Added first attempt at groups as facts

---
+++ lib/facter/accountfacts.rb
@@ -0,0 +1,18 @@
+require 'facter'
+require 'etc'
+
+Facter.add(:accountfacts_groups) do
+  confine :kernel do |value|
+    value == "Linux"
+  end
+  
+  setcode do
+    group_array = []
+    
+    Etc.group { |g|
+      group_array.push({"name" => g.name, "gid" => g.gid, "members" => g.mem})
+    }
+    
+    group_array
+  end
+end
