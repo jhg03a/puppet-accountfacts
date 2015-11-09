9b0954033397c2d5b18add7eb0ddb25e87843c35
Jonathan Gray <jhg03a@acu.edu>
2015-11-09 10:34:54 -0600

    Ran through rubocop to cleanup style

--- lib/facter/accountfacts.rb
+++ lib/facter/accountfacts.rb
@@ -2,16 +2,16 @@ require 'etc'
 
 Facter.add(:accountfacts_groups) do
   confine :kernel do |value|
-    value == "Linux"
+    value == 'Linux'
   end
-  
+
   setcode do
     group_array = []
-    
-    Etc.group { |g|
-      group_array.push({"name" => g.name, "gid" => g.gid, "members" => g.mem})
-    }
-    
+
+    Etc.group do |g|
+      group_array.push('name' => g.name, 'gid' => g.gid, 'members' => g.mem)
+    end
+
     group_array
   end
 end
