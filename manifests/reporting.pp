b5f68c50d41b812334d15aaebb4206c209593542
Jonathan Gray <jhg03a@acu.edu>
2015-12-09 14:46:54 -0600

    Fixed module naming

--- manifests/reporting.pp
+++ manifests/reporting.pp
@@ -1,15 +1,16 @@
 # Installs a script which handles puppet-accountfacts reporting
-class puppet-accountfacts::reporting (
+class accountfacts::reporting (
   $install_path = '/opt/puppet-accountfacts',
   $user = 'root',
-  $group = 'root'
-  
+  $group = 'root',
+){
+  include stdlib
   validate_absolute_path($install_path)
   
   file { $install_path :
-      mode   => '0750',
-      owner  => $user,
-      group  => $group,
-      source => "puppet:///modules/puppet-accountfacts/accountfacts.reporting.rb"
+    mode   => '0750',
+    owner  => $user,
+    group  => $group,
+    source => 'puppet:///modules/puppet-accountfacts/accountfacts.reporting.rb'
   }
-)
+}
